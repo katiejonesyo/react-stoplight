a832a3a99a7309268c1e807ed2004cc965a036f6
Katelinn Jones <katiejonesyo@gmail.com>
2021-04-22 16:34:41 -0700

    traffic light files finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import TrafficLight from "./components/TrafficLight";
+import TrafficLight from "./components/TrafficLight";
 // import TrafficLight from "./components/TrafficLightAutomata";
 
 import "./index.css";
 
 function App() {
-  // return <TrafficLight />;
+  return <TrafficLight />;
 }
 
 const rootElement = document.getElementById("root");
